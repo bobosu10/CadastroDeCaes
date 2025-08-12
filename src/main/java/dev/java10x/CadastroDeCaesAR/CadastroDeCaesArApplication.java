f25389c0e51d9263c0b2296ba7cc78c112d6ccc7
bobosu10 <lucasbarbosadasilva3016@gmail.com>
2025-08-11 21:49:55 -0300

    first commit

---
+++ src/main/java/dev/java10x/CadastroDeCaesAR/CadastroDeCaesArApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeCaesAR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeCaesArApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeCaesArApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/CadastroDeCaesAR/CadastroDeCaesArApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeCaesAR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeCaesArApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
