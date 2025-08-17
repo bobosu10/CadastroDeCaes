3aa2a17db46b1c91939ff413b13f9690ee4cc544
bobosu10 <lucasbarbosadasilva3016@gmail.com>
2025-08-17 19:13:17 -0300

    feat:mudanca no controle e adicionado o flyway

--- src/main/java/dev/java10x/CadastroDeCaesAR/Caes/Controller/DogController.java
+++ src/main/java/dev/java10x/CadastroDeCaesAR/Caes/Controller/DogController.java
@@ -1,15 +1,61 @@
 package dev.java10x.CadastroDeCaesAR.Caes.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class DogController {
 
+    //boas vindas
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Essa e minha primeira mensagem!";
     }
+
+    //adicionar(CREATE)
+    @PostMapping("/criar")
+    public String criar(){
+        return "Cão cadastrado.";
+    }
+
+    //procurar(CREATE)
+
+    //mostrar todos(READ)
+    @GetMapping("/todos")
+    public String mostrarTodos(){
+        return "Todos os cães";
+    }
+
+    //mostrar por id(READ)
+    @GetMapping("/mostrarPorID")
+    public String mostrarPorID(){
+        return "cão com id:";
+    }
+
+    //alterar dados(UPDATE)
+    @PutMapping("/alterar")
+    public String alterarDados(){
+        return "dados alterardos";
+    }
+
+    //deletar(DELETE)
+    @DeleteMapping("/deletar")
+    public String deletarCao(){
+        return "cão deletado";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/main/java/dev/java10x/CadastroDeCaesAR/Treinamentos/Controller/TreinamentoController.java
+++ src/main/java/dev/java10x/CadastroDeCaesAR/Treinamentos/Controller/TreinamentoController.java
@@ -1,4 +1,34 @@
 package dev.java10x.CadastroDeCaesAR.Treinamentos.Controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
 public class TreinamentoController {
+    //CRUD
+
+    @PostMapping("/criar")
+    public String criarTreino(){
+        return "Treino criado com sucesso";
+    }
+
+    @GetMapping("/mostrarTodosOsTreinos")
+    public String mostrarTreinos(){
+        return "todos os treinos:";
+    }
+
+    @GetMapping
+    public String mostrarPorID(){
+        return "treino por id:";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarDados(){
+        return "Dados alterados";
+    }
+
+    @DeleteMapping
+    public String deletarTreino(){
+        return "Treino deletado";
+    }
 }
